dcccf7da663d8883702ce94b0cb8f21481832d42
gk <ishkredov2@gmail.com>
2023-05-22 19:12:07 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+num = int(input('Input number\n'))
+if num == 1:
+    print('Понедельник')
+if num == 2:
+    print('Вторник')
+if num == 3:
+    print('Среда')
+if num == 4:
+    print('Четверг')
+if num == 5:
+    print('Пятница')
+if num == 6:
+    print('Суббота')
+if num == 7:
+    print('Воскресенье')
